1317073612338530b30bafdd60a6fbbe364c49ca
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-06-03 22:05:20 +0300

    Migrations
    Persons and Addresses table migrations

---
+++ database/migrations/2021_06_03_185636_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address',255);
+            $table->integer('post_code');
+            $table->string('city_name',255);
+            $table->string('country_name',255);
+            $table->foreignId('person_Id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
