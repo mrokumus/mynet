92d995796c2a5352ffbbfdfb2ab286b22003d1b3
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-06-04 01:22:52 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(1)->create();
-         \App\Models\Person::factory(50)->create();
-         \App\Models\Address::factory(50)->create();
+        \App\Models\User::factory(1)->create();
+        \App\Models\Person::factory(50)->create();
+        \App\Models\Address::factory(50)->create();
+        $this->call(BackendMenuSeeder::class);
     }
 }
