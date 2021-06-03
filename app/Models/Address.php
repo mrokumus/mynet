18545c57db16e0c3bc7898a195904ec9a36fa438
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-06-03 22:14:45 +0300

    Models
    Models created

---
+++ app/Models/Address.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $table = 'addresses';
+    protected $fillable = [
+        'address',
+        'post_code',
+        'city_name',
+        'country_name',
+    ];
+
+}
---
+++ app/Models/Person.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Person extends Model
+{
+    use HasFactory;
+
+    protected $table = 'persons';
+    protected $fillable = [
+        'name',
+        'birthday',
+        'gender'
+    ];
+
+
+    /**
+     * Get addresses for the person. (One to many relation)
+     * @return HasMany
+     */
+    public function getAddresses(): HasMany
+    {
+        return $this->hasMany(Address::class);
+    }
+}
