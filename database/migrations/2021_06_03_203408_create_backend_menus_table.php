98d55f83f3b258fd9eea7366af5971c12c54f64c
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-06-04 01:21:36 +0300

    Create 2021_06_03_203408_create_backend_menus_table.php

---
+++ database/migrations/2021_06_03_203408_create_backend_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBackendMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('backend_menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('icon');
+            $table->boolean('has_sub_menu')->default(0);
+            $table->integer('sub_menu_level')->default(1);
+            $table->integer('sub_menu_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('backend_menus');
+    }
+}
