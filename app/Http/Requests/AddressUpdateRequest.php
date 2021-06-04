bed12963c6fccd54af680c0a25623b3e2856480a
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-06-04 11:04:15 +0300

    Create AddressUpdateRequest.php
    Create request for validation

---
+++ app/Http/Requests/AddressUpdateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AddressUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (Auth::check()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "person_id" => "required|integer|exists:persons,id",
+            "address" => "required|string|min:10|max:255",
+            "post_code" => "required|string|min:2|max:20",
+            "city_name" => "required|string|min:2|max:50",
+            "country_name" => "required|string|min:4|max:50",
+        ];
+    }
+}
